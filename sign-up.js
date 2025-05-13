5fbd240308b70f3fbef40e6f106b8213350d7835
pikachu-24 <135854418+pikachu-24@users.noreply.github.com>
2025-05-13 17:20:30 +0300

    Update sign-up.js

--- sign-up.js
+++ sign-up.js
@@ -9,18 +9,24 @@ document.getElementById("create-account").addEventListener("submit", event => {
       // i'm just lazy and don't want to code a server.
       challenge: Uint8Array.from([1, 2, 3, 4]),
       rp: {
-        name: "Conditional UI Example"
+        name: "Conditional UI Example",
+        id: "https://webauthn-demo-git-main-pikachu-24s-projects.vercel.app",
       },
       user: {
         id: Uint8Array.from(username.split("").map(c => c.codePointAt(0))),
         name: username,
         displayName: name,
       },
-      pubKeyCredParams: [],
+       pubKeyCredParams: [
+    { type: "public-key", alg: -7 },  // ES256
+    { type: "public-key", alg: -257 }, // RS256
+  ],
+       timeout: 60000, // Try increasing timeout
+  attestation: "direct",
       authenticatorSelection: {
-        userVerification: "required",
-        residentKey: "required",
-        //authenticatorAttachment: "platform",
+        userVerification: "preffered",
+        requireResidentKey: true,
+        authenticatorAttachment: "cross-platform",
       },
     }
   }).then(credential => {
